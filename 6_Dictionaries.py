3caff359a9fb88a69a6eb7bf6e8a5227e4cc7ff9
Mint Monster <ronromaniuk@hotmail.com>
2022-01-19 18:01:02 -0500

    Tiny commit

---
+++ 6_Dictionaries.py
@@ -0,0 +1,11 @@
+#Chap 6 - Exercises (Dictionaries)
+
+#Create basic dictionary
+beattles = {}
+
+beattles['Paul'] = 'Bass'
+beattles['John'] = 'Guitar'
+beattles['George'] = 'Guitar'
+beattles['Ringo'] = 'Drums'
+
+print(beattles)
