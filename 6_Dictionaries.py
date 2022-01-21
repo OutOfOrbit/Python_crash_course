df9aa68683aec747e66fff9575077136689105dc
Mint Monster <ronromaniuk@hotmail.com>
2022-01-20 19:03:09 -0500

    mini-commit

--- 6_Dictionaries.py
+++ 6_Dictionaries.py
@@ -1,11 +1,28 @@
 #Chap 6 - Exercises (Dictionaries)
 
 #Create basic dictionary
-beattles = {}
+#beattles = {}
 
-beattles['Paul'] = 'Bass'
-beattles['John'] = 'Guitar'
-beattles['George'] = 'Guitar'
-beattles['Ringo'] = 'Drums'
+#beattles['Paul'] = 'Bass'
+#beattles['John'] = 'Guitar'
+#beattles['George'] = 'Guitar'
+#beattles['Ringo'] = 'Drums'
 
-print(beattles)
+#print(beattles)
+
+#Another dictionary
+allergies = {}
+
+allergies['Ron'] = 'crack'
+allergies['Bako'] = 'twits'
+allergies['Saholy'] = 'maifa'
+allergies['Willy'] = 'sneakers'
+allergies['Amber'] = 'king'
+
+for person,value in allergies.items():
+	dude = person
+	stuff = value
+	if person == 'Ron':
+		print(f"Hey {dude}, looks like you have a big {stuff.upper()}")
+	else:
+		print(f"Hey {dude}, looks like you don't like {stuff}")
