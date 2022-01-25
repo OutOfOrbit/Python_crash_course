181ce74df72b034b1cf6ce6e7f294ad4510b167d
Mint Monster <ronromaniuk@hotmail.com>
2022-01-25 18:52:07 -0500

    6_Dictionaries.py needs debugging

--- 6_Dictionaries.py
+++ 6_Dictionaries.py
@@ -10,7 +10,7 @@
 
 #print(beattles)
 
-#Another dictionary
+#Loop through key-value pairs using .item() method
 allergies = {}
 
 allergies['Ron'] = 'crack'
@@ -26,3 +26,19 @@ for person,value in allergies.items():
 		print(f"Hey {dude}, looks like you have a big {stuff.upper()}")
 	else:
 		print(f"Hey {dude}, looks like you don't like {stuff}")
+
+#Another dictionary exercise
+glossary = {}
+glossary['method'] = "function that relates @@ or is related to @@ an object"
+glossary['list'] = "a data-type that can contain a series of comma-separated values @@ Python version of an array @@"
+glossary['dictionary'] = "a data-type that can contain a series of key-value pairs @@ Python version of an object in JS @@"
+glossary['if'] = "a keyword, used at the start of a conditional statement"
+glossary['boolean'] = "data-type that can either be True or False @@ 1 or 0 @@"
+
+for key,value in glossary.items():
+	term = key
+	definition = value
+	print(term)
+	print(definition)
+
+
