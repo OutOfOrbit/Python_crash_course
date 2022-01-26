d9ccb6e39dbeb0b68dd9d5a9bc4aa4a8dc4d73a9
OutOfOrbit <ronromaniuk@hotmail.com>
2022-01-26 08:49:22 -0500

    finally getting repo on OldRedDeb lined up with the upstream!

---
+++ 5_if_statements.py
@@ -0,0 +1,32 @@
+#Chapter 5 - "if" statements
+
+#Very basic start
+#car = 'subaru'
+#print("Is car == 'subaru'? I predict True.")
+#print(car == 'subaru')
+
+#print ("\nIs car == 'audi'? I predict False.")
+#print(car == 'audi')
+
+#Testing alien colors
+#alien_2 = 'green'
+#alien_3 = 'yellow'
+
+#if alien_1 == 'blue':
+#	print("Yep! Right color!")
+#else:
+#	print("Nope.  Better luck next time, dufus!")
+
+#Another if statement example (list of user names)
+#users = ['John', 'Paul', 'George', 'Ringo', 'Admin']
+
+#for user in users:
+#	if user == 'Admin':
+#		print(f"Hey {user.upper()}, get to work!")
+#	else:
+#		print(f"Hey {user.title()}, you made some AWESOME music!")
+
+#Test to see if list is empty
+users = []
+
+
