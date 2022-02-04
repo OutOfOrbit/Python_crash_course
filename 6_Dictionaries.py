01807ba9b9b57d8e88f1cdf10d35c7ba35fc4c19
Mint Monster <ronromaniuk@hotmail.com>
2022-02-04 09:29:40 -0500

    mini

--- 6_Dictionaries.py
+++ 6_Dictionaries.py
@@ -1,6 +1,6 @@
-#Chap 6 - Exercises (Dictionaries)
+# Chap 6 - Exercises (Dictionaries)
 
-#Create basic dictionary
+# Create basic dictionary
 #beattles = {}
 
 #beattles['Paul'] = 'Bass'
@@ -8,9 +8,9 @@
 #beattles['George'] = 'Guitar'
 #beattles['Ringo'] = 'Drums'
 
-#print(beattles)
+# print(beattles)
 
-#Loop through key-value pairs using .item() method
+# Loop through key-value pairs using .item() method
 #allergies = {}
 
 #allergies['Ron'] = 'crack'
@@ -19,7 +19,7 @@
 #allergies['Willy'] = 'sneakers'
 #allergies['Amber'] = 'king'
 #
-#for person,value in allergies.items():
+# for person,value in allergies.items():
 #	dude = person
 #	stuff = value
 #	if person == 'Ron':
@@ -27,16 +27,16 @@
 #	else:
 #		print(f"Hey {dude}, looks like you don't like {stuff}")
 
-#Another dictionary exercise
-glossary = {}
-glossary['method'] = 'function that relates, or is related to, an object'
-glossary['list'] = 'a data-type that can contain a series of comma-separated values (Python version of an array)' 
-glossary['dictionary'] = 'a data-type that can contain a series of key-value pairs (Python version of an object in JS)'
-glossary['if'] = 'a keyword, used at the start of a conditional statement'
-glossary['boolean'] = 'data-type that can either be True or False (1 or 0)'
-
-for key,value in glossary.items():
-	term = key
-	definition = value
-	print(f"**{term.upper()}**")
-	print(definition + '\n\t')
+# Another dictionary exercise
+#glossary = {}
+#glossary['method'] = 'function that relates, or is related to, an object'
+#glossary['list'] = 'a data-type that can contain a series of comma-separated values (Python version of an array)'
+#glossary['dictionary'] = 'a data-type that can contain a series of key-value pairs (Python version of an object in JS)'
+#glossary['if'] = 'a keyword, used at the start of a conditional statement'
+#glossary['boolean'] = 'data-type that can either be True or False (1 or 0)'
+#
+# for key,value in glossary.items():
+#	term = key
+#	definition = value
+#	print(f"**{term.upper()}**")
+#	print(definition + '\n\t')
