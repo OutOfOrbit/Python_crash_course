2e04502d8b3b300d464926f8b72b2f0db561b2cf
Mint Monster <ronromaniuk@hotmail.com>
2022-02-05 15:25:24 -0500

    mini

--- 6_Dictionaries.py
+++ 6_Dictionaries.py
@@ -27,16 +27,23 @@
 #	else:
 #		print(f"Hey {dude}, looks like you don't like {stuff}")
 
-# Another dictionary exercise
+# Cross-referrencing a dictionary & a list 
 #glossary = {}
 #glossary['method'] = 'function that relates, or is related to, an object'
 #glossary['list'] = 'a data-type that can contain a series of comma-separated values (Python version of an array)'
 #glossary['dictionary'] = 'a data-type that can contain a series of key-value pairs (Python version of an object in JS)'
 #glossary['if'] = 'a keyword, used at the start of a conditional statement'
 #glossary['boolean'] = 'data-type that can either be True or False (1 or 0)'
+##
+#ref_list = []
+#ref_list.append('list')
+##
+#for key,value in glossary.items():
+#	if key in ref_list:
+#		print(f"**{key.upper()}**")
+#		print(value.upper() + '\n\t')
+#	else:
+#		print(f"--{key.lower()}--")
+#		print(value + '\n\t')
 #
-# for key,value in glossary.items():
-#	term = key
-#	definition = value
-#	print(f"**{term.upper()}**")
-#	print(definition + '\n\t')
+    
